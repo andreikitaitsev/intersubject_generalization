f7cc9be414ed068b3c370057182901d9d96940a9
Kitaitsev <andre.kit17@gmail.com>
2021-05-28 10:54:24 +0200

    Reduce the number of epochs to run faster

--- dnn/perceiver/EEG/run/hpar_grid-proj_head-ver2.sh
+++ dnn/perceiver/EEG/run/hpar_grid-proj_head-ver2.sh
@@ -16,7 +16,7 @@ declare -a out_dirs
 # Non variable params
 lr=0.0001
 batch_size=64
-n_epochs=400
+n_epochs=200
 bpl=50
 out_dir="/scratch/akitaitsev/intersubject_generalization/dnn/perceiver/EEG/leftthomas/projection_head/hpar-grid-ver2/"
 names=("perc_latent_array_dim" "perc_num_latent_dim" "perc_latent_heads" "perc_depth" "out_dim_ENC" "out_dim_PH")

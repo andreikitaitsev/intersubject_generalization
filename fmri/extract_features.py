a76f1947ff4306faea128b0dd7118f2cb80af29f
Kitaitsev <andre.kit17@gmail.com>
2021-11-12 14:49:40 +0100

    Debugged and working

--- fmri/extract_features.py
+++ fmri/extract_features.py
@@ -3,6 +3,7 @@
 import torch
 import numpy as np
 import joblib
+import tqdm
 from pathlib import Path
 from decord import VideoReader
 from decord import cpu, gpu 
@@ -65,24 +66,29 @@ def load_fmri(base_dir, track, region, dataset, subjs=None):
             fmri.append(fl["test_data"])
     return fmri
 
-def extract_features_cornet_s(video_list, model):
+def extract_features_cornet_s(video_list, model, debug=False):
     '''
+    Extract intermediate features of CORnet-S. By default, uses all the timesteps of 
+    V1, V2, V4, IT, decoder layers, sublayer = output.
+
+    Timesteps per layer in CORnet-S:
+    # V1 --> 1 time step
+    # V2 --> 2 time steps
+    # V4 --> 4 time steps
+    # IT --> 2 time steps
+    # decoder --> 1 time step
+    
     Inputs:
-        ims -list of PIL images sampled from one video of shape (each of shape (height,width, chs))
-        lists of str:
-        layers - layers of cornets, default=['V1', 'V2', 'V4', 'IT', 'decoder']
-        sublayers - sublayers of cornets, default = ['output']
-        timesteps - timesteps of cornets, default = [0, 1, 2, 3]
-            Timesteps per layer in CORnet-S:
-            # V1 --> 1 time step
-            # V2 --> 2 time steps
-            # V4 --> 4 time steps
-            # IT --> 2 time steps
-            # decoder --> 1 time step
+        video_list - list of lists of PIL images sampled from one videos by sample_video_from_mp4  
+        model - pytorch CORnet-S model (https://github.com/dicarlolab/CORnet)
+        debug - bool. If true, prints the shape the output of each layer, sublayer and timestep.
+            Default=False
     Outputs:
-        stacked_acts
+        activations - 2d numpy array of shape (n_videos, concat_features), where concat_features
+            are the concatenated outputs of all CORnet-S layers and timesteps averaged over frames 
+            for each video.
     '''
-    def _extract_features_single_video(ims, model, layers=None, sublayers=None, timesteps=None):
+    def _extract_features_single_video_single_layer(ims, model, layer):
         # resize normalize images
         resize_normalize = trn.Compose([
             trn.Resize((224,224)),
@@ -95,12 +101,8 @@ def extract_features_cornet_s(video_list, model):
             model.cuda()
             ims=ims.cuda().to(torch.float32)
 
-        if layers is None:
-            layers = ['V1', 'V2', 'V4', 'IT','decoder']
-        if sublayers is None:
-            sublayers = ['output']
-        if timesteps is None:
-            timesteps = [0, 1, 2, 3] 
+        timestep_dict={'V1': [0], 'V2':[0,1], 'V4':[0,1,2,3],'IT':[0,1],'decoder':[0]}
+        sublayer = 'output'
 
         # hooker function to access the intermediate model features of particular 
         #layer and sublayer
@@ -111,32 +113,72 @@ def extract_features_cornet_s(video_list, model):
             
         model_feats=[]
         activations=defaultdict()
-        for layer in layers:
-            for sublayer in sublayers:
-                try:
-                    m = model.module
-                except:
-                    m = model
-                model_layer = getattr(getattr(m, layer), sublayer)
-                model_layer.register_forward_hook(_store_features)
-                _model_feats = []
-                with torch.no_grad():
-                    model(ims)
-                for timestep in timesteps:
-                    try:
-                        model_feats.append(_model_feats[timestep])
-                        print('Layer '+str(layer)+' sublayer '+str(sublayer)+' timestep '+str(timestep)\
-                        +' '+str(_model_feats[timestep].shape))
-                    except:
-                        print('Layer has '+str(len(_model_feats))+' timesteps')
-        return None            
-    def _extract_features_multiple_videos(video_list, model, layers=None, sublayers=None, timesteps=None):
-        for video in video_list: 
-            _extract_features_single_video(video, model, layers=None, sublayers=None, timesteps=None)
-        return None
+        try:
+            m = model.module
+        except:
+            m = model
+        model_layer = getattr(getattr(m, layer), sublayer)
+        model_layer.register_forward_hook(_store_features)
+        _model_feats = []
+        with torch.no_grad():
+            model(ims)
+        timesteps=timestep_dict[str(layer)]
+        for timestep in timesteps:
+            try:
+                model_feats.append(_model_feats[timestep])
+                if debug:
+                    print('Layer: '+str(layer)+' sublayer: '+str(sublayer)+' timestep: '+str(timestep)\
+                    +'\n'+str(_model_feats[timestep].shape))
+            except:
+                if debug:
+                    print('Attempted to access timestep '+str(timestep)+\
+                        'but \n layer '+str(layer)+' has '+str(len(timesteps))+' timesteps.')
+        # concatenate along features if a layer has multiple time steps. If layer has one time step, 
+        # convert it to np array
+        if len(model_feats) > 1:
+            model_feats=np.concatenate(model_feats, axis=1)
+        elif len(model_feats) == 1:
+            model_feats=model_feats[0]
+        return model_feats            
 
-    _extract_features_multiple_videos(video_list, model)
-    return None
+    def _extract_features_single_video_multiple_layers(ims, model, layers=None):
+        '''Extract features from multiple layers for one video, averages features over frames for every 
+        layer and concatenates features from different layers
+        Inputs:
+            ims - np array of shape (n_frames, features)
+            model - torch model, CORnet-S
+            layers - list of str, default = V1, V2, V4, IT, decoder
+        Outputs:
+            acts - 1d np.array of concatenated activations for every layer averaged over frames 
+        '''
+        if layers is None:
+            layers = ('V1', 'V2', 'V4', 'IT', 'decoder')
+        acts=[]
+        for layer in layers:
+            # average activations over frames for each layer
+            acts.append(np.mean(_extract_features_single_video_single_layer(ims, model, layer), axis=0))
+        acts = np.concatenate(acts)
+        return acts
+
+    def _extract_features_multiple_videos(video_list, model, layers=None):
+        '''Extract features from every video and return array of shape (n_videos, concat_features)
+        Inputs:
+            video_list - list of lists of PIL images sampled from different videos
+            model - torch model, CORnet-S
+            layers - list of layers to use. Default - ['V1', 'V2', 'V4', 'IT', 'decoder']
+        Outputs:
+            activations - 2d numpy array of shape (n_videos, n_features), where n_features are features
+                of different layers and timesteps of cornet-s averaged over frames for every video and 
+                concatenated 
+        '''
+        activations=[]
+        for video in tqdm(video_list): 
+            activations.append(_extract_features_single_video_multiple_layers(video, model, layers))
+        activations=np.stack(activations, axis=0)
+        return activations
+
+    activations = _extract_features_multiple_videos(video_list, model)
+    return activations
 
 
 class feature_extractor(object):
@@ -153,16 +195,30 @@ class feature_extractor(object):
 
         feature_extraction_func - model-specific function to use for feature extraction.
             Default - extract_features_cornet_s
+        posprocessor - object, shall implement .fit_transform or .fit and .transform method.
+            these methods will be called on the ouput of the feature extraction function.
+            Assumed to be sklearn transformer or Pipeline object. Default=None-no posprocessing
+    Methods:
+        __call__ - applies feature_extraction function on the input data.
         '''
-    def __init__(self, model, feature_extraction_function=None):
+    def __init__(self, model, feature_extraction_function=extract_features_cornet_s,\
+        postprocessor=None):
+        self.feature_extraction_function = feature_extraction_function
         self.model = model
-        if feature_extraction_function is None:
-            self.feature_extraction_function = extract_features_cornet_s
-        else:
-            self.feature_extraction_function = feature_extraction_function 
+        self.postprocessor = postprocessor
 
     def __call__(self, data):
         activations = self.feature_extraction_function(data, self.model)
+        if not self.postprocessor is None:
+            # check if postprocessor has fit_transform or fit and transform methods   
+            if getattr(self.postprocessor, 'fit_transform', False) and\
+                callable(getattr(self.postprocessor, 'fit_transform'):
+                activations = self.postprocessor.fit_transform(activations)
+            elif getattr(self.postprocessor, 'fit', False) and getattr(self.postprocessor, 'transform', False)\
+                and getattr(self.postprocessor, 'fit') and getattr(self.postprocessor, 'transform')):
+                self.postprocessor.fit(activations)
+                activations = self.postprocessor.transform(activations)
+        return activations
 
 
 if __name__ == '__main__':
@@ -172,10 +228,12 @@ if __name__ == '__main__':
     import argparse
     import cornet
     import glob
+    from sklearn.decomposition import PCA
     parser=argparse.ArgumentParser()
     parser.add_argument('-video_dir', type=str, help='Directory where videos are stored')
     parser.add_argument('-model', type=str, default='s', help='Type of cornet to use. s, r, or z.'
     'Default=s.')
+    parser.add_argument('-n_comp', type=int, help='Number of Principal Components to retain in data.')
     args = parser.parse_args()
 
     def get_cornet_model(pretrained=True, map_location=None):
@@ -189,14 +247,14 @@ if __name__ == '__main__':
     video_list = glob.glob(args.video_dir + '/*.mp4')
     video_list.sort()
 
-    feature_extractor = feature_extractor(model)
+    # configure feature extractor
+    postprocessor = PCA(args.n_comp)
+    feature_extractor = feature_extractor(model, extract_features_cornet_s, postprocessor)
+
     videos=[]
-#    for video in video_list:
-#        vid, frames=sample_video_from_mp4(video)
-#        videos.append(vid)
-    video, frames=sample_video_from_mp4(video_list[0])
-    videos.append(video)
-    feature_extractor(videos)
-
-
+    for video in video_list:
+        vid, frames=sample_video_from_mp4(video)
+        videos.append(vid)
+    activations = feature_extractor(videos)
+    print(activations.shape)
 
